e4fc9bb3e67db88f86384f3dc6a9ebe6affacc8b
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-05-09 17:25:50 -0500

    [ADD] Mail HTML

---
+++ src/utils/mensajeHTML.js
@@ -0,0 +1,71 @@
+const generarMensajeHTML = (cita, horario) => {
+  return  `<!DOCTYPE html>
+        <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f9f9f9;
+              margin: 0;
+              padding: 0;
+            }
+        
+            .container {
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 20px;
+              background-color: #ffffff;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+        
+            .header {
+              background-color: #fbcce7 ;
+              padding: 20px;
+              text-align: center;
+            }
+        
+            .content {
+              padding: 20px;
+            }
+        
+            .message {
+              margin-bottom: 20px;
+              text-align: center;
+            }
+        
+            .message p {
+              font-size: 16px;
+              line-height: 1.5;
+              color: #333333;
+              text-align: center;
+            }
+        
+            .footer {
+              background-color: #fbcce7 ;
+              padding: 20px;
+              text-align: center;
+              font-size: 14px;
+              color: #666666;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="header">
+              <h1>Confirmación de cita</h1>
+            </div>
+            <div class="content">
+              <div class="message">
+                <p>¡Hola! Tu cita ha sido agendada para el <strong>${cita.fecha}</strong> a las <strong>${horario.hora}</strong>.</p>
+              </div>
+            </div>
+            <div class="footer">
+              <p>No responda a este correo electrónico. Para cualquier consulta, póngase en contacto con nuestro equipo de atención al cliente.</p>
+            </div>
+          </div>
+        </body>
+        </html>
+        `;
+}
+
+module.exports = {generarMensajeHTML}
