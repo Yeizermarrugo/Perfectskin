e895486cbc643f0c035443c63b7cd0afaf3c0eaf
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-05-09 17:27:08 -0500

    [MOD] require

--- src/auth/auth.controlle.js
+++ src/auth/auth.controlle.js
@@ -1,20 +1,20 @@
-const {getUserByEmail, createUser} = require('../users/user.controller')
-const {comparePassword} = require('../utils/crypt')
+const { getUserByEmail } = require('../users/user.controller')
+const { comparePassword } = require('../utils/crypt')
 
-const loginUser = async (email, password) =>{
+const loginUser = async (email, password) => {
     //? user.password = contraseña hasheada
     //* password = contraseña en texto plano
-try {
-    const user = await getUserByEmail(email)
-    const verifyPassword = comparePassword(password, user.password)
-    if(verifyPassword){
-        return user
-    } else {
+    try {
+        const user = await getUserByEmail(email)
+        const verifyPassword = comparePassword(password, user.password)
+        if (verifyPassword) {
+            return user
+        } else {
+            return false
+        }
+    } catch (error) {
         return false
     }
-} catch (error) {
-    return false
-}
 }
 
 
